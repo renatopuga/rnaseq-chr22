3612dead84525695834446ac2fa9da037e9295e9
Renato Puga <renatopuga@gmail.com>
2021-03-08 19:52:24 -0300

    Update run.star.rsem.sh

--- run.star.rsem.sh
+++ run.star.rsem.sh
@@ -41,19 +41,3 @@ do
 	RNASEQ_data/rsem.$(basename $fastqR1 _1.fq.gz)/rsem; 
 done
 
-# Expression table merge by genes
-cd RNASEQ_data
-
-mkdir gene-level
-cd gene-level
-ls -d1 ../rsem.* | gawk '{print("ln -s",$1"/rsem.genes.results",gensub("../rsem.","","g",$1))}' | sh
-cd ..
-time R --file=../run.merge.files.R --args gene-level 5 gene-level-5
-
-# Expression table merge by isoforms
-mkdir transcript-level
-cd transcript-level
-ls -d1 ../rsem.* | gawk '{print("ln -s",$1"/rsem.genes.results",gensub("../rsem.","","g",$1))}' | sh
-cd ..
-time R --file=../run.merge.files.R --args transcript-level 5 transcript-level
-
